eccab404453c84c5c569af8b247a5ffd041098f7
Alejandro <alejandro.g.surio@outlook.es>
2024-11-13 23:32:10 +0100

    Continuing learning Rust

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let no_variable = 10;
     println!("My variable no variable contians: {}", no_variable);
@@ -15,5 +17,19 @@ fn main() {
 
     const PERMANENCE: i32 = 2;
     println!("For sure this value never changes: {}", PERMANENCE);
+
+    println!("Can you comment your opinion about variables?");
+    let mut answer = String::new();
+
+    io::stdin()
+        .read_line(&mut answer)
+        .expect("Error reading the line");
     
+    println!("Your opinion is {}", answer);
+
+    fn waving (name: String) {
+        println!("Hello {}", name)
+    }
+
+    waving("Alejandro".to_string())
 }
